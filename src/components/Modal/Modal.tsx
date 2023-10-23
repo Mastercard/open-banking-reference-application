c519882f8e250f1ef35b37f9bf53757f6760e7f1
Ashish Choure <Ashish.Choure@mastercard.com>
2023-10-23 17:56:28 +0530

    - Resolved Code Smells

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import { Box, Link, Stack, Typography } from '@mui/material';
@@ -12,90 +12,85 @@ export default function Modal() {
     };
 
     return (
-        <React.Fragment>
-            <Dialog
-                open={open}
-                aria-labelledby='alert-dialog-title'
-                aria-describedby='alert-dialog-description'
-            >
-                <Box className='modal-dialog-parent'>
-                    <Box className='modal-dialog-content'>
-                        <Typography
-                            variant='h6'
-                            className='flex !font-semibold'
-                        >
-                            <img
-                                src='/mc_symbol.svg'
-                                alt='Open Banking'
-                                className='w-[44px]'
-                            />{' '}
-                            developers
+        <Dialog
+            open={open}
+            aria-labelledby='alert-dialog-title'
+            aria-describedby='alert-dialog-description'
+        >
+            <Box className='modal-dialog-parent'>
+                <Box className='modal-dialog-content'>
+                    <Typography variant='h6' className='flex !font-semibold'>
+                        <img
+                            src='/mc_symbol.svg'
+                            alt='Open Banking'
+                            className='w-[44px]'
+                        />{' '}
+                        developers
+                    </Typography>
+                    <Box>
+                        <Typography className='ref-text'>
+                            REFERENCE APP
                         </Typography>
-                        <Box>
-                            <Typography className='ref-text'>
-                                REFERENCE APP
-                            </Typography>
-                            <Typography className='open-bannking-text'>
-                                Open Banking
-                            </Typography>
-                            <Typography className=''>
-                                Easily connect your customer's financial data to
-                                your product.
-                            </Typography>
-                            <Stack
-                                direction='row'
-                                spacing={1}
-                                alignItems='flex-start'
-                                sx={{ mt: 1.5 }}
-                            >
-                                <Link
-                                    variant='caption'
-                                    href='https://developer.mastercard.com/open-banking-us/documentation'
-                                    target='_blank'
-                                    className='!no-underline'
-                                    fontWeight={'bold'}
-                                    color={'#111'}
-                                >
-                                    Learn more about the product
-                                </Link>
-                                <img
-                                    src='/utility.svg'
-                                    style={{ marginTop: '1px' }}
-                                    alt=''
-                                />
-                            </Stack>
-                        </Box>
-
-                        <Stack direction='row' className='gap-2'>
-                            <Button
-                                variant='text'
-                                onClick={handleClose}
-                                className='demo__button flex items-center'
-                            >
-                                View demo
-                            </Button>
-                            <a
-                                href='https://developer.mastercard.com'
-                                rel='noreferrer'
+                        <Typography className='open-bannking-text'>
+                            Open Banking
+                        </Typography>
+                        <Typography className=''>
+                            Easily connect your customer's financial data to
+                            your product.
+                        </Typography>
+                        <Stack
+                            direction='row'
+                            spacing={1}
+                            alignItems='flex-start'
+                            sx={{ mt: 1.5 }}
+                        >
+                            <Link
+                                variant='caption'
+                                href='https://developer.mastercard.com/open-banking-us/documentation'
                                 target='_blank'
+                                className='!no-underline'
+                                fontWeight={'bold'}
+                                color={'#111'}
                             >
-                                <Button
-                                    endIcon={
-                                        <ExternalIcon
-                                            data={'#F37338'}
-                                            background={'#FFF'}
-                                        />
-                                    }
-                                    variant='outlined'
-                                    className='view-on-github__button flex items-center'
-                                >
-                                    View on Github
-                                </Button>
-                            </a>
+                                Learn more about the product
+                            </Link>
+                            <img
+                                src='/utility.svg'
+                                style={{ marginTop: '1px' }}
+                                alt=''
+                            />
                         </Stack>
                     </Box>
+
+                    <Stack direction='row' className='gap-2'>
+                        <Button
+                            variant='text'
+                            onClick={handleClose}
+                            className='demo__button flex items-center'
+                        >
+                            View demo
+                        </Button>
+                        <a
+                            href='https://developer.mastercard.com'
+                            rel='noreferrer'
+                            target='_blank'
+                        >
+                            <Button
+                                endIcon={
+                                    <ExternalIcon
+                                        data={'#F37338'}
+                                        background={'#FFF'}
+                                    />
+                                }
+                                variant='outlined'
+                                className='view-on-github__button flex items-center'
+                            >
+                                View on Github
+                            </Button>
+                        </a>
+                    </Stack>
                 </Box>
-            </Dialog>
-        </React.Fragment>
+            </Box>
+        </Dialog>
     );
 }
